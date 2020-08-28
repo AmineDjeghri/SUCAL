05cc457663ac2661efd2b05d70906308063bd670
aminedjeghri <aminedjeghri1@gmail.com>
2020-08-29 00:42:10 +0200

    changed structure of the folders

--- app/__init__.py
+++ app/__init__.py
@@ -28,11 +28,11 @@ def create_app():
         return User.query.get(int(user_id))
 
     # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+    from .auth_views import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
     # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+    from .main_views import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
