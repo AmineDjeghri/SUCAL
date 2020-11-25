9e1301ece26ff77eb3634ebd619d2fbb9629575c
modfort <mehdi-098@outlook.fr>
2020-11-25 20:14:27 +0100

    add some regex for controlling authenfication

--- app/auth_views.py
+++ app/auth_views.py
@@ -4,6 +4,7 @@ from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required, current_user
 
+import re
 
 auth = Blueprint('auth', __name__)
 
@@ -20,6 +21,15 @@ def login_post():
     password = request.form.get('password')
     remember = True if request.form.get('remember') else False
 
+    regmail = re.compile("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+")
+    regpassword = re.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$")
+    
+    if(regmail.match(email) == None):
+        flash('Error in Email.', 'warning')
+        return redirect(url_for('auth.login'))
+   
+    if(regpassword.match(password) == None):
+        flash("error in password","warning")
     user = User.query.filter_by(email=email).first()
     if not user or not check_password_hash(user.password, password):
         flash('Invalid login details.', 'danger')
@@ -40,6 +50,23 @@ def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
     password = request.form.get('password')
+
+
+    regmail = re.compile("[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+")
+    regname = re.compile("^[a-z0-9_-]{3,15}$")
+    regpassword = re.compile("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$")
+    
+    if(regmail.match(email) == None):
+        flash('Error in Email.', 'warning')
+        return redirect(url_for('auth.signup'))
+
+    if(regname.match(name) == None):
+        flash('Error in name',"warning")
+        return redirect(url_for('auth.signup') )         
+    
+    if(regpassword.match(password)==None):
+        flash("Error in password","warning")
+        return redirect(url_for('auth.signup'))
 
     user = User.query.filter_by(email=email).first() 
     if user: 
